fc38cd8eb4ed727e92ddb47d4514a5f145c63eda
Jeraldine Silvera <jeraldinesilvera@gmail.com>
2018-03-08 16:26:55 -0400

    pick date screen

--- js/angular/app.js
+++ js/angular/app.js
@@ -174,11 +174,11 @@ beetApp.controller('TryNowController', [ '$scope', '$compile', '$timeout', 'uiCa
     };
     /* event source that contains custom events on the scope */
     $scope.events = [
-        {title: 'Name',start: new Date(y, m, 1)},
-        {title: 'Name',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-        // {id: 999,title: 'Name',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-        {id: 999,title: 'Name',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-        {title: 'Name',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
+        // {title: 'Name',start: new Date(y, m, 1)},
+        // {title: 'Name',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+        // // {id: 999,title: 'Name',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+        // {id: 999,title: 'Name',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+        // {title: 'Name',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
         // {title: 'Name',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
     ];
     /* event source that calls a function on every view switch */
@@ -194,9 +194,9 @@ beetApp.controller('TryNowController', [ '$scope', '$compile', '$timeout', 'uiCa
         color: '#f00',
         textColor: 'yellow',
         events: [
-            {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-            {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
-            {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+            // {type:'party',title: 'Lunch',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+            // {type:'party',title: 'Lunch 2',start: new Date(y, m, d, 12, 0),end: new Date(y, m, d, 14, 0),allDay: false},
+            // {type:'party',title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
         ]
     };
     /* alert on eventClick */
