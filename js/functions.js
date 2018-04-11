b7c37d3bf799d6ca98da5f2ceb20d88c9532cb3d
Jeraldine Silvera <jeraldinesilvera@gmail.com>
2018-04-11 17:53:04 -0400

    last changes

--- js/functions.js
+++ js/functions.js
@@ -6,8 +6,38 @@ function ocultar() {
     document.getElementById("sidebar").style.width = "0";
 }
 
+function menuShow() {
+    document.getElementById("sidebar-horizontal").style.height = "500px";
+    $("#show").css("display","none");
+    $("#hide").css("display","block");
+    $("#sidebar-horizontal").css("background-color","#2f2f2e");
+}
+
+function menuHide() {
+    document.getElementById("sidebar-horizontal").style.height = "0";
+    $("#hide").css("display","none");
+    $("#show").css("display","block");
+    $("#sidebar-horizontal").css("background-color","#fff");
+}
+
+// function ocultar() {
+//     document.getElementById("sidebar").style.width = "0";
+// }
+
 function cascade() {
     $("#cascade li").css({
+        left: -800,
+        position: "relative"
+    }).each(function (i) {
+        var el = $(this);
+        setTimeout(function () {
+            el.animate({
+                left: 0
+            }, 500);
+        }, i * 200);
+    });
+
+    $("#cascade-horizontal li").css({
         left: -800,
         position: "relative"
     }).each(function (i) {
